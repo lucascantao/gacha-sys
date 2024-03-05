df8a32a26cc681f10f2e0364db0d7510ce536308
José Lucas dos Santos Cantão <cantao162@gmail.com>
2024-03-05 08:18:11 -0300

    fix data.sql

--- gacha-api/src/main/resources/data.sql
+++ gacha-api/src/main/resources/data.sql
@@ -27,7 +27,7 @@ INSERT INTO character (tier, bio, name) VALUES(5, 'Geo Archon', 'Zhongli');
 INSERT INTO character (tier, bio, name) VALUES(5, 'Hydro Archon', 'Furina');
 INSERT INTO character (tier, bio, name) VALUES(5, 'Knights of Favonious Leader', 'Jean');
 INSERT INTO character (tier, bio, name) VALUES(4, 'Favonious Maid Knight', 'Noelle');
-INSERT INTO character (tier, bio, name) VALUES(4, 'Unlucky Boy', 'Bennet');
+INSERT INTO character (tier, bio, name) VALUES(4, 'Unlucky Boy', 'Bennett');
 INSERT INTO character (tier, bio, name) VALUES(4, 'Cryon Num', 'Rosaria');
 INSERT INTO character (tier, bio, name) VALUES(4, 'Favonious Alchemist', 'Sucrose');
 INSERT INTO character (tier, bio, name) VALUES(5, 'Firecracker Girl', 'Yoimiya');
