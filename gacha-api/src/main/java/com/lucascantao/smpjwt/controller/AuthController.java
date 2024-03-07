7eb1e109f9b4175838d6847fb680a31c7479ad63
José Lucas dos Santos Cantão <cantao162@gmail.com>
2024-03-07 10:35:42 -0300

    register change

--- gacha-api/src/main/java/com/lucascantao/smpjwt/controller/AuthController.java
+++ gacha-api/src/main/java/com/lucascantao/smpjwt/controller/AuthController.java
@@ -57,6 +57,7 @@ public class AuthController {
         user.setName(register.getName());
         user.setEmail(register.getEmail());
         user.setPassword(passwordEncoder.encode(register.getPassword()));
+        user.setPulls(30);
 
         user = userRepository.save(user);
 
